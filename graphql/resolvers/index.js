3210ad90428b4b12fa72a38434916c14eec1d2f8
thetajwar2003 <rahmant7@bxscience.edu>
2021-01-15 12:10:39 -0500

    add and delete posts

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -7,6 +7,7 @@ module.exports = {
         ...postResolvers.Query
     },
     Mutation: {
-        ...usersResolvers.Mutation
+        ...usersResolvers.Mutation,
+        ...postResolvers.Mutation
     }
-}
+};

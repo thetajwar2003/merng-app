ee7d92818e1a033df482c08c611119d425620a8c
thetajwar2003 <rahmant7@bxscience.edu>
2021-01-15 09:31:42 -0500

    added schemas

--- index.js
+++ index.js
@@ -2,20 +2,35 @@ const { ApolloServer }  = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
 const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-    type Query{
-        sayHi: String!
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
+
+    type Query {
+        getPosts: [Post]
     }
 `;
 
 // process the query and return what the query returns
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello World!!!' 
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch(err) {
+                throw new Error(err);
+            }
+        } 
     }
-}
+};
 
 const server = new ApolloServer({
     typeDefs,
@@ -26,7 +41,7 @@ const server = new ApolloServer({
 mongoose.Promise = global.Promise
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
-    console.log("Connected to mongodb");
+    console.log("Connected to MongoDB");
     // initialize the server
     return server.listen({ port: 5000 });
 })

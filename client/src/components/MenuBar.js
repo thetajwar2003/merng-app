10d964377595d1f309c7622b705aa119fd608a9d
thetajwar2003 <rahmant7@bxscience.edu>
2021-01-27 13:49:51 -0500

    integrated authentication globally

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+    const { user, logout } = useContext(AuthContext);
     // determines what page you are on
     const pathname = window.location.pathname;
     const path = pathname === '/' ? 'home' : pathname.substr(1);
@@ -11,8 +14,22 @@ function MenuBar() {
 
     const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
-        <Menu pointing secondary size="massive" color="teal">
+    const menuBar = user ? 
+    (<Menu pointing secondary size="massive" color="teal">
+        <Menu.Item
+            name={user.username}
+            active
+            as={Link}
+            to="/"
+        />
+        <Menu.Menu position='right'>
+            <Menu.Item
+                name='logout'
+                onClick={logout}
+            />
+        </Menu.Menu>
+    </Menu>)
+        : (<Menu pointing secondary size="massive" color="teal">
             <Menu.Item
                 name='home'
                 active={activeItem === 'home'}
@@ -36,8 +53,9 @@ function MenuBar() {
                     to="/signup"
                 />
             </Menu.Menu>
-        </Menu>
-    )
+        </Menu>)
+
+    return menuBar;
 }
 
 export default MenuBar;
